6bc6a4472764af0123fb0797bf9564e61b8c01d7
Tatiana3557 <65028112+Tatiana3557@users.noreply.github.com>
2020-07-28 19:28:43 +0300

    optimize

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//include('alert.js')
+//include('slick.min.js')
+
+
+//функція підтримки зображень: webpcss
+function testWebP(callback) {
+    var webP = new Image(); webP.onload = webP.onerror = function () { callback(webP.height == 2); }; webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    
+    }
+    
+    testWebP(function (support) {
+    if (support == true) { document.querySelector('body').classList.add('webp'); }else{ document.querySelector('body').classList.add('no-webp'); }
+    
+    });
+    
+
+    
+
+function fontsStyle(params) {
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss'); if (file_content == '') { fs.writeFile(source_folder + '/scss/fonts.scss', '', cb); return fs.readdir(path.build.fonts, function (err, items) { if (items) { let c_fontname; for (var i = 0; i < items.length; i++) { let fontname = items[i].split('.'); fontname = fontname[0]; if (c_fontname != fontname) { fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb); } c_fontname = fontname; } } }) }
+    
+    }
+    
+
+    $(function(){
+        $('.slider').slick({
+            nextArrow: '<button class="slick-arorow slick-next"><img src="img/next.svg" alt="next arrow"></button>',
+            prevArrow: '<button class="slick-arorow slick-prev"><img src="img/prev.svg" alt="prev arrow"></button>',
+            fade: true,
+            responsive: [
+                {
+                    breakpoint: 441,
+                    settings: {
+                        arrows: false
+                    }
+                }
+            ]
+        });
+    });
+    
